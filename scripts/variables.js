5667ea9c1608e140bffada5a153900943b26d718
--global <--global>
2021-11-02 16:19:08 -0600

    yes

--- scripts/variables.js
+++ scripts/variables.js
@@ -1,11 +1,17 @@
-
 // variables are named containers for stuff
 
 // let is a keyword to declare variables
 // age is the name of the variable
 // 10000 is the value assigned to the variable age
 let age = 10000; // the data type of age is a number
-let fullName = "Epic person"; // the data type of fullName is string
+let fullName = `Epic person`; // the data type of fullName is string
 
-let camelCasingNameFormat = "hi";
-let SNAKE_CASING_FORMAT = "hi";
+let camelCasingNameFormat = `hi`;
+let SNAKE_CASING_FORMAT = `hi`;
+
+let isActive = true; //boolean values are true or false
+
+age = "16"; // age is now a string
+age = false; // age is now a boolean
+
+const MAX_AGE = 200;
