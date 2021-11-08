6161a5c8c114fd4a2bf758c5107a9a1c6b784958
--global <--global>
2021-11-08 16:24:50 -0700

    Rectangle

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,12 @@
 /** @type {HTMLCanvasElement} */
-const canvas = document.getElementById('game-canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
+
+let x = 300;
+let y = 200;
+
+ctx.fillRect(x, y, 200, 200);
+
+ctx.arc(x, y, 50, 0, Math.PI * 2);
