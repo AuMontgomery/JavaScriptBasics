8a0558a3a194304d283793bbad5ac77a06d6e2ff
--global <--global>
2021-11-10 16:26:16 -0700

    Colors

--- scripts/app.js
+++ scripts/app.js
@@ -16,10 +16,38 @@ let y2 = 100;
 
 let color = 0;
 
+let lightness = 50;
+let isDrawing = false;
+
+window.addEventListener("keydown", (e) => {
+	console.log(e.key);
+
+	switch (e.key) {
+		case "e":
+			lightness = 100;
+			break;
+
+		case "g":
+			color = 120;
+		default:
+			lightness = 50;
+	}
+});
+
+canvas.addEventListener("mousedown", () => {
+	isDrawing = true;
+});
+
+canvas.addEventListener("mouseup", () => {
+	isDrawing = false;
+});
+
 canvas.addEventListener("mousemove", (e) => {
-	ctx.fillStyle = `hsla(${color}, 90%, 60%, 1`;
+	if (!isDrawing) return;
+
+	ctx.fillStyle = `hsla(${color}, 90%, ${lightness}%, 1`;
 	ctx.beginPath();
-	ctx.arc(e.offsetX, e.offsetY, 60, 0, Math.PI * 2, false);
+	ctx.arc(e.offsetX, e.offsetY, 10, 0, Math.PI * 2, false);
 	ctx.fill();
-	color = color + 0.1;
+	//color = color + 0.5;
 });
